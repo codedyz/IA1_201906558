ae5d67a5a5ff4b4b37a3f918e0d6166153dbdb05
EDDY FERNANDO DIAZ GALINDO <3044397700114@ingenieria.usac.edu.gt>
2024-08-05 23:20:21 -0600

    Tarea1

---
+++ reflex_agent_norepeat.js
@@ -0,0 +1,32 @@
+function reflex_agent(location, state) {
+    if (state == "DIRTY") return "CLEAN";
+    else if (location == "A") return "RIGHT";
+    else if (location == "B") return "LEFT";
+}
+
+function test(states, visiteds) {
+    var location = states[0];
+    var state = location == "A" ? states[1] : states[2];
+    var action_result = reflex_agent(location, state);
+    
+	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+
+    if (action_result == "CLEAN") {
+        if (location == "A") states[1] = "CLEAN";
+        else if (location == "B") states[2] = "CLEAN";
+    } else if (action_result == "RIGHT") {
+        states[0] = "B";
+    } else if (action_result == "LEFT") {
+        states[0] = "A";
+    }
+
+    var state_key = states.join();
+    if (!visiteds[state_key]) {
+        visiteds[state_key] = true;
+        setTimeout(function () { test(states, visiteds); }, 1000);
+    }
+}
+
+var states = ["A", "DIRTY", "DIRTY"];
+var visiteds = {};
+test(states, visiteds);
